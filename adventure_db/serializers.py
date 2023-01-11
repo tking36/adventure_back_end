02190a464716a1bef2301d320ecccd45b2d73988
Taylor King <taylorking@Taylors-MBP.attlocal.net>
2023-01-11 12:59:11 -0500

    Fixing for Heroku

--- adventure_db/serializers.py
+++ adventure_db/serializers.py
@@ -4,4 +4,4 @@ from .models import Adventure
 class AdventureSerializer(serializers.ModelSerializer): # serializers.ModelSerializer just tells django to convert sql to JSON
     class Meta:
         model = Adventure # tell django which model to use
-        fields = ('id', 'health', 'attack', 'accuracy', 'weapons', 'items', 'villians') # tell django which fields to include
+        fields = ('id', 'health', 'attack', 'accuracy', 'weapons', 'items', 'villains') # tell django which fields to include
